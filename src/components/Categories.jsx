d51fdb61e8ea6fb8bea68bdb352cc44a71fcfbcb
cantwell <rjazantsev@gmail.com>
2021-01-31 13:40:54 +0300

    Added Categories component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <ul className="categories">
+            <li className="active">Все</li>
+            <li>Мясные</li>
+            <li>Вегетарианская</li>
+            <li>Гриль</li>
+            <li>Острые</li>
+            <li>Закрытые</li>
+        </ul>
+    )
+}
+
+export default Categories;
