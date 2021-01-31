ed5e18578d811e5b4bb8433e93bbe4de6b8bd0eb
cantwell <rjazantsev@gmail.com>
2021-01-31 13:43:35 +0300

    appending new components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Header} from './components/';
+import {Header, Categories, Sort} from './components/';
 
 function App() {
   return (
@@ -8,29 +8,8 @@ function App() {
         <div className="content">
             <div className="container">
               <nav className="nav">
-                <ul className="categories">
-                    <li className="active">Все</li>
-                    <li>Мясные</li>
-                    <li>Вегетарианская</li>
-                    <li>Гриль</li>
-                    <li>Острые</li>
-                    <li>Закрытые</li>
-                </ul>
-                <div className="sort">
-                    <p>
-                        <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z" fill="#2C2C2C"/>
-                        </svg>
-                        Сортировка по: <span className="sort__chosen">популярности</span>
-                    </p>
-                    <div className="sort__popup">
-                        <ul>
-                            <li className="active">популярности</li>
-                            <li>по цене</li>
-                            <li>по алфавиту</li>
-                        </ul>
-                    </div>
-                </div>
+                <Categories/>
+                <Sort/>
               </nav>
             </div>
             <div className="pizzas">
