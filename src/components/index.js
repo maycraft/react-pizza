ddb05a609aada1f3a7c846a03b3a50821d3624ae
cantwell <rjazantsev@gmail.com>
2021-01-31 13:43:10 +0300

    appended export files

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
-
-export {default as Header} from './Header'; 
+export {default as Header} from './Header/Header'; 
+export {default as Categories} from './Categories'; 
+export {default as Sort} from './Sort/Sort'; 
+export {default as Popup} from './Sort/Popup';
+export {default as Button} from './Header/Button';
