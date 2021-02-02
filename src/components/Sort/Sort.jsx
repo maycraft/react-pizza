e20b8caa30aa22bb889c87aabf8cd7ad9dda3903
cantwell <rjazantsev@gmail.com>
2021-02-02 15:00:10 +0300

    output sort select item

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Sort = ({items}) => {
     //состояние скрывать/показывать Popup
     const [isVisiblePopup, setIsVisiblePopup] = React.useState(false);
-    const [isSelect, setIsSelect] = React.useState(0); 
+    const [selectItem, setSelectItem] = React.useState(0); 
     //создаём ref-переменную для получения ссылки на элемнт sort
     const softRef = React.useRef();
     
@@ -18,7 +18,8 @@ const Sort = ({items}) => {
     }, []);
 
     const onSelectItem = idx => {
-        setIsSelect(idx);
+        setSelectItem(idx);
+        setIsVisiblePopup(false);
     }
     
     const togglePopup = () => {
@@ -32,14 +33,14 @@ const Sort = ({items}) => {
                 <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z" fill="#2C2C2C"/>
                 </svg>
-                Сортировка по: <span className="sort__chosen" onClick={togglePopup}>популярности</span>
+                Сортировка по: <span className="sort__chosen" onClick={togglePopup}>{items[selectItem]}</span>
             </p>
             { isVisiblePopup && <div className="sort__popup">
                                     <ul>
                                         {
                                             items.map( (item, idx) => (
                                                 <li key={`item-${idx}`} 
-                                                    className={ isSelect === idx ? "active" : ""} 
+                                                    className={ selectItem === idx ? "active" : ""} 
                                                     onClick={ () => onSelectItem(idx) } >{item}</li>
                                             ))
                                         }
