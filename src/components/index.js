dee3ede12c72b9978244aebbff46e71c22bfd0f3
cantwell <rjazantsev@gmail.com>
2021-02-02 14:50:15 +0300

    del component Popup

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export {default as Header} from './Header/Header'; 
 export {default as Categories} from './Categories'; 
 export {default as Sort} from './Sort/Sort'; 
-export {default as Popup} from './Sort/Popup';
 export {default as Button} from './Header/Button';
