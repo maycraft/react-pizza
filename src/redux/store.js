52f8975e04a93f85f25872939bab58bf9a8e055a
cantwell <rjazantsev@gmail.com>
2021-02-05 15:21:57 +0300

    append thunk in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,9 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import {pizzas as pizzasReducer, filters as filtersReducer} from './reducers';
-
-const rootReducer = combineReducers({
-    pizzas: pizzasReducer,
-    filters: filtersReducer     
-})
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware() ));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk) ));
 
 export default store;
