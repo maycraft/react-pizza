17e36ddb482014aa44c062d5e8093d4adb72595e
cantwell <rjazantsev@gmail.com>
2021-02-05 11:46:09 +0300

    passing index in onSelectItems

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
 
-const Categories =  React.memo( () => {
+const Categories =  React.memo( ({items, onSelectItem}) => {
 
-    
-    const categories = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
     const [active, setActive] = useState(null);
 
     const onSelectCategory = idx => {
         setActive(idx);
+        onSelectItem(idx);
     }
     return (
         <ul className="categories">
             <li onClick={ () => {onSelectCategory(null)} }
                 className={active === null ? "active" : ""}>Все</li>
-            { categories.map( (category, idx) => (
+            { items.map( (category, idx) => (
                 <li onClick={ () => {onSelectCategory(idx)} } 
                     className={active === idx ? "active" : ""} 
                     key={idx}>{category}</li>
