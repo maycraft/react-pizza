29ac2c6c8c84439fbad3bf4c4e9e5262f50df964
cantwell <rjazantsev@gmail.com>
2021-02-08 17:13:10 +0300

    append filed order in sortItems

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Categories, Sort } from '.'
 import {fetchPizzas, setIsLoadedAC} from '../redux/actions/pizzas';
-import {setCategoryIndexAC, setSortByAC} from '../redux/actions/filters';
+import {setCategoryIndexAC, setIndexSortByAC} from '../redux/actions/filters';
 import {LoaderItem, PizzaItem} from './PizzaItem/';
 
 
 const categories = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 const sortItems = [
-    {name: 'популярности', type: 'popular'},
-    {name: 'по цене', type: 'price'}, 
-    {name: 'по алфавиту', type: 'alphabet'}
+    {name: 'популярности', type: 'rating', order: 'desc'},
+    {name: 'по цене', type: 'price', order: 'asc'}, 
+    {name: 'по алфавиту', type: 'title', order: 'asc'}
 ];
 
 const Main = () => {
@@ -19,19 +19,21 @@ const Main = () => {
     const pizzas = useSelector( ({pizzas}) => pizzas.items );
     const isLoaded = useSelector( ({pizzas}) => pizzas.isLoaded );
     const categoryIndex = useSelector( ({filters}) => filters.categoryIndex );
-    const sortBy = useSelector( ({filters}) => filters.sortBy );
+    const indexSortBy = useSelector( ({filters}) => filters.indexSortBy );
+
+    const objectSortBy = sortItems[indexSortBy];
 
     React.useEffect(() => {
         dispatch( setIsLoadedAC(false) );
-        dispatch(fetchPizzas(categoryIndex));
-    }, [dispatch, categoryIndex]);
+        dispatch(fetchPizzas(categoryIndex, objectSortBy));
+    }, [dispatch, categoryIndex, indexSortBy, objectSortBy]);
 
     const handleSelectCategoty = idx => {
         dispatch( setCategoryIndexAC(idx) );
     }
 
-    const handleSortBy = name => {
-        dispatch( setSortByAC(name) );
+    const handleIndexSortBy = idx => {
+        dispatch( setIndexSortByAC(idx) );
     }
 
     return (
@@ -39,7 +41,7 @@ const Main = () => {
             <div className="container">
                 <nav className="nav">
                     <Categories items={categories} categoryIndex={categoryIndex}  onSelectCategory={handleSelectCategoty} />
-                    <Sort items={ sortItems } sortBy={sortBy} onSortBy={handleSortBy}/>
+                    <Sort items={ sortItems } indexSortBy={indexSortBy} onIndexSortBy={handleIndexSortBy}/>
                 </nav>
             </div>
             <div className="pizzas">
@@ -48,7 +50,7 @@ const Main = () => {
                     {
                         isLoaded
                         ? pizzas.map( item => (<PizzaItem key={ item.id } {...item}/>) )
-                        : Array(12).fill(0).map( ( _, idx) => ( <LoaderItem key={idx}/> ) )
+                        : Array(12).fill(0).map( ( _, idx) => ( <LoaderItem key={idx} /> ) )
                     }
                     
                 </div>
