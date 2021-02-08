830460184066b99f74c81cc2b2f9969c9d2f9d85
cantwell <rjazantsev@gmail.com>
2021-02-08 15:05:46 +0300

    created setIsLoadedAC

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -7,8 +7,16 @@ export const setPizzasAC = (items) => {
     }
 }
 
+export const setIsLoadedAC = ( isLoading ) => {
+    return {
+        type: 'SET_IS_LOADED',
+        payload: isLoading
+    }
+}
+
 export const fetchPizzas = () => (dispatch) => {    
     API.fetchPizzas().then( response => {
         dispatch( setPizzasAC(response.data) );
+        dispatch( setIsLoadedAC(true) );
     })
 }
