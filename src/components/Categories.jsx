e4a363b17fa3abfdcd72782a4ab5e1ec009165ae
cantwell <rjazantsev@gmail.com>
2021-02-08 13:34:46 +0300

    processing active item from redux

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,21 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 
-const Categories =  React.memo( ({items, onSelectItem}) => {
+const Categories =  React.memo( ({items, categoryIndex, onSelectCategory}) => {
 
-    const [active, setActive] = useState(null);
-
-    const onSelectCategory = idx => {
-        setActive(idx);
-        onSelectItem(idx);
-    }
     return (
         <ul className="categories">
             <li onClick={ () => {onSelectCategory(null)} }
-                className={active === null ? "active" : ""}>Все</li>
+                className={categoryIndex === null ? "active" : ""}>Все</li>
             { items.map( (category, idx) => (
                 <li onClick={ () => {onSelectCategory(idx)} } 
-                    className={active === idx ? "active" : ""} 
+                    className={categoryIndex === idx ? "active" : ""} 
                     key={idx}>{category}</li>
                 )
             )}
@@ -25,7 +19,8 @@ const Categories =  React.memo( ({items, onSelectItem}) => {
 
 Categories.propTypes = {
     items: PropTypes.arrayOf(PropTypes.string.isRequired),
-    onSelectItem: PropTypes.func.isRequired
+    categoryIndex: PropTypes.number,
+    onSelectCategory: PropTypes.func.isRequired
 }
 
 export default Categories;
