c4427b28497acf340f4dd762f956a666baa00e8a
cantwell <rjazantsev@gmail.com>
2021-02-08 09:24:20 +0300

    export Sort in index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export {default as Header} from './Header/Header'; 
 export {default as Categories} from './Categories'; 
-export {default as Sort} from './Sort/Sort'; 
+export {default as Sort} from './Header/Sort'; 
 export {default as Button} from './Header/Button';
 export {default as Main} from './Main';
 export {default as Cart} from './Cart';
