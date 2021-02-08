118c64dfe672437997e1e7becb00433546ec52e5
cantwell <rjazantsev@gmail.com>
2021-02-08 13:35:13 +0300

    processing sort element from redux

--- src/components/Header/Sort.jsx
+++ src/components/Header/Sort.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-const Sort = React.memo(({items, onSortBy}) => {
+const Sort = React.memo(({items, sortBy, onSortBy}) => {
         //состояние скрывать/показывать Popup
         const [isVisiblePopup, setIsVisiblePopup] = React.useState(false);
-        const [selectItem, setSelectItem] = React.useState(0); 
         //создаём ref-переменную для получения ссылки на элемнт sort
         const softRef = React.useRef();
+        //Получаем объект, зная тип сортировки
+        const sortByObj = items.find( item => item.type === sortBy );
         
         /*Единажды при рендеренге, вешаем событие click на body, для отслеживания нажатия и если это не .sort,
         скрываем popup */
@@ -25,7 +26,6 @@ const Sort = React.memo(({items, onSortBy}) => {
         }, []);
 
         const onSelectItem = idx => {
-            setSelectItem(idx);
             setIsVisiblePopup(false);
             onSortBy(items[idx].type);
         }
@@ -43,14 +43,14 @@ const Sort = React.memo(({items, onSortBy}) => {
                         width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z" fill="#2C2C2C"/>
                     </svg>
-                    Сортировка по: <span className="sort__chosen" onClick={togglePopup}>{items[selectItem].name}</span>
+                    Сортировка по: <span className="sort__chosen" onClick={togglePopup}>{sortByObj.name}</span>
                 </p>
                 { isVisiblePopup && <div className="sort__popup">
                                         <ul>
                                             {
                                                 items.map( (item, idx) => (
                                                     <li key={ item.type + '-' + idx} 
-                                                        className={ selectItem === idx ? "active" : ""} 
+                                                        className={ item.type === sortBy ? "active" : ""} 
                                                         onClick={ () => onSelectItem(idx) } >{item.name}</li>
                                                 ))
                                             }
@@ -63,6 +63,7 @@ const Sort = React.memo(({items, onSortBy}) => {
 )
 Sort.propTypes = {
     items: PropTypes.arrayOf(PropTypes.object.isRequired),
+    sortBy: PropTypes.string.isRequired,
     onSortBy: PropTypes.func.isRequired
 }
 
