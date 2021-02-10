bdc38046ba2a7fe63f44bff2b426240582a29112
cantwell <rjazantsev@gmail.com>
2021-02-10 15:00:51 +0300

    append props in Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import cn from 'classnames';
+import PropTypes from 'prop-types';
 
-const Button = ({ outline, className, children }) => {
+const Button = React.memo(({ outline, className, children, onAddPizzaToCart }) => {
     return (
         <button className= { cn( 'button', className, {
                 'button--outline': outline 
             })}
+            onClick={onAddPizzaToCart}
         >
             {children}
         </button>
     )
+})
+
+Button.prototype = {
+    outline: PropTypes.bool,
+    onAddPizzaToCart: PropTypes.func
 }
 
 export default Button;
