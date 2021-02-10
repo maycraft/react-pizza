01ce2711b9cb4dbe1e5024a340d541a60e9c69a2
cantwell <rjazantsev@gmail.com>
2021-02-10 15:04:53 +0300

    passing add new pizza to redux and displey in btn

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import cn from 'classnames';
+import { Button } from '..';
+import { setAddPizzaAC } from '../../redux/actions/cart';
 
-const PizzaItem = React.memo(({imageUrl, title, price, sizes, types}) => {
+const PizzaItem = React.memo(({id, imageUrl, title, price, sizes, types, dispatch, pizzasToCart}) => {
+
+    console.log();
 
     const availableTypes = ['тонкое', 'традиционное'];
     const availableSizes = [26, 30, 40];
 
-    const [activeType, setActiveType] = React.useState(types[0]);
+    const [activeIndexType, setActiveIndexType] = React.useState(types[0]);
     const [activeSize, setActiveSize] = React.useState( sizes[0]);
 
     const onSelectItem = idx => {
@@ -15,7 +19,21 @@ const PizzaItem = React.memo(({imageUrl, title, price, sizes, types}) => {
     }
 
     const onSelectType = idx => {
-        setActiveType(idx);
+        setActiveIndexType(idx);
+    }
+
+    const activeType = availableTypes[activeIndexType];
+
+    const handleAddPizzaToCart = () => {
+        const pizza = {
+            id,
+            title,
+            imageUrl,
+            price,
+            activeType,
+            activeSize
+        }
+        dispatch(setAddPizzaAC(pizza));
     }
 
     return (
@@ -31,7 +49,7 @@ const PizzaItem = React.memo(({imageUrl, title, price, sizes, types}) => {
                             <div 
                                     key={idx}
                                     className={cn('item__type', {
-                                        'active': idx === activeType,
+                                        'active': idx === activeIndexType,
                                         'disabled': !types.includes(idx)
                                     })}
                                     onClick={() => onSelectType(idx)}    
@@ -55,14 +73,16 @@ const PizzaItem = React.memo(({imageUrl, title, price, sizes, types}) => {
             </div>
             <div className="item__order">
                 <div className="item__price">от {price} ₽</div>
-                <div className="button button--outline button--add">
+                <Button className="button--add"
+                        onAddPizzaToCart={handleAddPizzaToCart}
+                        outline>
                     <svg width="12" height="12" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" fill="#EB5A1E"/>
                         <path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z" fill="#EB5A1E"/>
                     </svg>
                     <span> Добавить</span>
-                    <span className="amount">2</span>
-                </div>
+                    { pizzasToCart[id] && <span className="amount">{pizzasToCart[id].length}</span> }
+                </Button>
             </div>
         </div>
     )
