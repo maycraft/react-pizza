4162cad2ff62baccd21f8738a5f0bd3c9213b742
cantwell <rjazantsev@gmail.com>
2021-02-12 10:00:22 +0300

    using proxy in webpack

--- src/redux/dal/api.js
+++ src/redux/dal/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = {
     fetchPizzas: (categoryIndex, {type, order}) => {
         const category = categoryIndex === null ? '' : `category=${categoryIndex}`;        
-        return axios.get(`http://localhost:3001/pizzas?${category}&_sort=${type}&_order=${order}`);
+        return axios.get(`/pizzas?${category}&_sort=${type}&_order=${order}`);
     }
 }
 
