19da21d2772366feb2a15924067c7864d687f351
cantwell <rjazantsev@gmail.com>
2021-02-12 10:50:09 +0300

    export CartItem in index

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export {default as Header} from './Header/Header';
 export {default as Categories} from './Categories'; 
 export {default as Sort} from './Header/Sort'; 
 export {default as Main} from './Main';
-export {default as Cart} from './Cart';
+export {default as Cart} from './Cart/Cart';
 export {default as Button} from './Button';
+export {default as CartItem} from './Cart/CartItem';
