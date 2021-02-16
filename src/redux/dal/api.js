36f3967afd4885d84a5ed0d0030796d546f9bd0f
cantwell <rjazantsev@gmail.com>
2021-02-16 12:04:39 +0300

    changed api for getting object args from thunk

--- src/redux/dal/api.js
+++ src/redux/dal/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const API = {
-    fetchPizzas: (categoryIndex, {type, order}) => {
+    fetchPizzas: ({categoryIndex, objectSortBy}) => {
+
         const category = categoryIndex === null ? '' : `category=${categoryIndex}`;        
-        return axios.get(`/pizzas?${category}&_sort=${type}&_order=${order}`);
+        return axios.get(`/pizzas?${category}&_sort=${objectSortBy.type}&_order=${objectSortBy.order}`);
     }
 }
 
