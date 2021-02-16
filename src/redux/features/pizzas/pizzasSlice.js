8b65b20a1de928df7fa4327b0042ae1c1ff830cf
cantwell <rjazantsev@gmail.com>
2021-02-16 12:02:45 +0300

    moved pizzasSlice file

---
+++ src/redux/features/pizzas/pizzasSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import API from '../../dal/api';
+
+export const fetchPizzasByCategory = createAsyncThunk(
+    'pizzas/fetchAllPizzas',
+    async (objArgs, thunkAPI) => {
+        const response = await API.fetchPizzas(objArgs);
+        thunkAPI.dispatch(setIsLoaded(true));
+        return response.data;
+    }
+)
+
+const pizzasSlice = createSlice({
+    name: 'pizzas',
+    initialState: {
+        items: [],
+        isLoaded: false
+    },
+    reducers: {
+        setIsLoaded( state, action ) {
+            state.isLoaded = action.payload;
+        }
+    },
+    extraReducers: {
+        [fetchPizzasByCategory.fulfilled]: ( state, action ) => {
+            state.items = action.payload;
+        }
+    }
+})
+export const { setIsLoaded } = pizzasSlice.actions;
+
+export default pizzasSlice.reducer;
