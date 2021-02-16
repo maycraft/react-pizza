ea3f55d732d2c2655b5a46693056b0da4cd19f3f
cantwell <rjazantsev@gmail.com>
2021-02-16 12:29:05 +0300

    added filtersSlice to rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
-import pizzasReducer from '../features/pizzas/pizzasSlice'
+import pizzasReducer from '../features/pizzas/pizzasSlice';
+import filtersReducer from '../features/filters/filtersSlice';
 
-import filters from './filters';
 import cart from './cart'
 
 const rootReducer = combineReducers({ 
     pizzas: pizzasReducer, 
-    filters, 
+    filters: filtersReducer,
     cart });
 
 export default rootReducer;
