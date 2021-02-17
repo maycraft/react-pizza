f44591f9a31714dca88c286866446dbd37e5750d
cantwell <rjazantsev@gmail.com>
2021-02-17 09:32:18 +0300

    create deletePizza in cartSlice

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -1,5 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const getCombineArr = ( obj ) => [].concat.apply([], Object.values(obj));
+const getTotalPrice = ( arr ) => arr.reduce( (sum, item) => sum + item.price, 0 ); 
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
@@ -13,17 +16,24 @@ const cartSlice = createSlice({
             ? state.items[action.payload.id].push(action.payload)
             : state.items[action.payload.id] = [action.payload];
 
-            const allPizzas = [].concat.apply([], Object.values(state.items));
+            // const allPizzas = [].concat.apply([], Object.values(state.items));
+            const allPizzas = getCombineArr(state.items);
             state.totalCount = allPizzas.length;
-            state.totalPrice = allPizzas.reduce( (sum, item) => sum + item.price, 0 );
+            state.totalPrice = getTotalPrice(allPizzas);
         },
         clearCart( state, action ){
             state.items = {};
             state.totalCount = 0;
             state.totalPrice = 0;
+        },
+        deletePizza( state, action ){
+            delete state.items[action.payload];
+            const allPizzas = getCombineArr(state.items);
+            state.totalCount = allPizzas.length;
+            state.totalPrice = getTotalPrice(allPizzas);
         }
     }
 })
 
-export const { addPizza, clearCart } = cartSlice.actions;
+export const { addPizza, clearCart, deletePizza } = cartSlice.actions;
 export default cartSlice.reducer;
